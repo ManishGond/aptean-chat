605bab7540ae1f63c555a71af0750e3a7cc47cd0
Manish Gond <manish.n.gond@gmail.com>
2024-12-05 17:12:00 +0530

    MIU04 - Added background image, made a glassmorphism for the Login Page, Updated the index.css

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="flex flex-col items-start justify-center min-w-96 mx-auto">
+      <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+        <h1 className="text-3xl font-bold text-center text-gray-300">
+          Login •<span className="text-purple-300 font-bold"> APTEAN</span>
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
